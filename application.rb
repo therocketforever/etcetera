c362a0f9e87233dd0819856768e79374a2466726
DON who is a ghost <therocketforever@me.com>
2013-03-13 12:36:51 -0400

    Slim template for layout & index

--- application.rb
+++ application.rb
@@ -1,4 +1,25 @@
 #encoding: UTF-8
 
 class Application < Sinatra::Base
+  enable :inline_templates
+
+  get '/' do
+    slim :index
+  end
 end
+
+__END__
+
+@@layout
+doctype html
+html
+  head
+    title Slim Investigation
+
+  body
+    == yield
+
+  footer
+
+@@index
+h1 Hello From Slim!
