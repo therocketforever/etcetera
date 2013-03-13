3afa9af0f07588cd1c38450444f3fc1b21f7cca4
DON who is a ghost <therocketforever@me.com>
2013-03-13 15:51:19 -0400

    integration attempt one

--- application.rb
+++ application.rb
@@ -7,12 +7,24 @@ class Application < Sinatra::Base
   configure(:development){Bundler.require(:development)}
   configure(:test){}
 
+  helpers Sinatra::Handlebars
+
+  get '/style.css' do
+    content_type :css, :charset => "utf-8"
+    scss :style
+  end
+  
+	get '/templates' do
+		content_type 'application/javascript'
+		handlebar 'public/templates/**/*.hbs'
+	end
+  
   get '/' do
     slim :index
   end
 end
 
-Binding.pry
+#Binding.pry
 __END__
 
 @@layout
@@ -23,6 +35,10 @@ html
 
   body
     == yield
+
+  script src="js/ember-1.0.0-rc.1.js"
+  script src="templates"
+  script src="app/app.js"
 
   footer
     p Wow! I am a footer!!
--- config.ru
+++ config.ru
@@ -1,6 +1,7 @@
 require 'bundler'
 
 Bundler.require
+require File.expand_path(File.dirname(__FILE__) + '/handlebars')
 
 require "./application"
 run Application
